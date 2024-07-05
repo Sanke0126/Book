940ff940ee64d67ff66dd4463268ecd54193afcb
Sanke0126 <nambaashdee@gmail.com>
2024-07-05 12:59:00 +0800

    aaa

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -9,29 +9,29 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
   return (
     <>
       <Head title="Home" />
-      <div className="h-[80px] w-full flex items-center justify-between px-8 selection:bg-[#FF2D20] selection:text-white">
-        <div className="text-[24px] hover:text-black/70 focus:outline-none">
+      <div className="h-[80px] w-full flex items-center justify-between bg-zinc-100 text-gray-500 px-8 selection:bg-[#FF2D20] selection:text-white">
+        <div className="text-[20px] hover:text-black/70 focus:outline-none">
           <Link href="/"> Book </Link>
         </div>
-        <nav className="text-[20px]">
+        <nav className="text-[18px]">
           {auth.user ? (
             <Link
               href={route("dashboard")}
-              className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+              className="rounded-md px-3 py-2 text-gray-500 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
             >
               Dashboard
             </Link>
           ) : (
-            <section className="flex">
+            <section className="flex ">
               <Link
                 href={route("login")}
-                className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                className="rounded-md px-3 py-2 text-gray-500 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
               >
                 Log in
               </Link>
               <Link
                 href={route("register")}
-                className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                className="rounded-md px-3 py-2 text-gray-500 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
               >
                 Register
               </Link>
